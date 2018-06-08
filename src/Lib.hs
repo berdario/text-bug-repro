83686c456366869322cfad2cf2848538c48d7d97
Dario Bertini <berdario@google.com>
2018-06-08 18:19:37 +0200

    Cleanup most files

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,14 @@
 module Lib
-    ( someFunc
+    ( readTheSame
     ) where
 
+import qualified Data.Text    as T
 import qualified Data.Text.IO as T
 
-someFunc :: IO ()
-someFunc = T.putStrLn =<< T.readFile "testfile"
+filename = "testfile"
+
+readTheSame :: IO Bool
+readTheSame = do
+    content1 <- T.pack <$> readFile filename
+    content2 <- T.readFile filename
+    return $ content1 == content2
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import           Lib
+import           Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Data.Text.IO.readFile" $ do
+      it "should read the same contents as Prelude.readFile" $ do
+        result <- readTheSame
+        result `shouldBe` True
